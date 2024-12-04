5ac195a84db486440e51019b60a803658f3f2b84
kindnemo <namanmishraepicstar@gmail.com>
2024-12-04 23:06:50 +0530

    Initiates the project Takes an input converts each char to ascii converts the ascii list to decimal and then to binary

---
+++ main.py
@@ -0,0 +1,16 @@
+# Define the text
+text = str(input("Enter Text: "))
+
+# Convert each character to its ASCII value
+ascii_values = [ord(char) for char in text]
+
+# Print the ASCII values
+print(ascii_values)
+
+# Convert the decimal into binary 
+def decimal_to_binary(decimal_list):
+    binary_list = [format(num, '08b') for num in ascii_values]
+    return binary_list
+
+binary_nums = decimal_to_binary(ascii_values)
+print(binary_nums)
